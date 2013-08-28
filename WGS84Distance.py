736dd93408e588ee851beb0e4dc7757146e327fe
Wang Kang <wangk@startest.net>
2013-08-28 23:32:58 +0800

    add a example of distance(radius) logger

---
+++ WGS84Distance.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# Haversine formula example in Python
+# Author: Wayne Dyck
+
+import math
+
+def distance(origin, destination):
+    """
+    (lat,lon)
+    kilometers
+    """
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+    radius = 6371 # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
--- fetchgps.py
+++ fetchgps.py
@@ -2,35 +2,53 @@ from pynmea import nmea
 import serial
 import os
 from WGS84ToGCJ02 import transform
+from WGS84Distance import distance
+
+#import threading
+
+#class ThreadWorker(threading.Thread):
+    #def __init__(self, callable, *args, **kwargs):
+        #super(ThreadWorker, self).__init__()
+        #self.callable = callable
+        #self.args = args
+        #self.kwargs = kwargs
+
+    #def run(self):
+        #self.callable(*self.args, **self.kwargs)
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
-ser = serial.Serial("COM2",38400)
-while True:
-    line = ser.readline()
-    if(line[4] == 'G'): # $GPGGA
-        if(len(line) > 50):
-            #print line
-            gpgga = nmea.GPGGA()
-            gpgga.parse(line)
-            lats = gpgga.latitude
-            longs = gpgga.longitude
-            
-            #convert degrees,decimal minutes to decimal degrees 
-            _lat = (float(lats[2]+lats[3]+lats[4]+lats[5]+lats[6]+lats[7]+lats[8]))/60
-            lat = (float(lats[0]+lats[1])+_lat)
-            _long = (float(longs[3]+longs[4]+longs[5]+longs[6]+longs[7]+longs[8]+longs[9]))/60
-            longs = (float(longs[0]+longs[1]+longs[2])+_long)
-            
-            #calc position
-            pos_y = lat
-            pos_x = -longs #longitude is negaitve
+def decodeNMEAStream(serialport,GCJ02=False):
+    while True:
+        line = serialport.readline()
+        if(line[4] == 'G'): # $GPGGA
+            if(len(line) > 50):
+                #print line
+                gpgga = nmea.GPGGA()
+                gpgga.parse(line)
+                lats = gpgga.latitude
+                longs = gpgga.longitude
+                
+                #convert degrees,decimal minutes to decimal degrees 
+                _lat = (float(lats[2]+lats[3]+lats[4]+lats[5]+lats[6]+lats[7]+lats[8]))/60
+                lat = (float(lats[0]+lats[1])+_lat)
+                _long = (float(longs[3]+longs[4]+longs[5]+longs[6]+longs[7]+longs[8]+longs[9]))/60
+                longs = (float(longs[0]+longs[1]+longs[2])+_long)
 
-    #import code
-    #code.interact(local=locals())
-    #cls()
-        #print "Lat: %s  Long: %s    Alt: %s    Sats: %s"%(gpgga.latitude,gpgga.longitude,gpgga.antenna_altitude,gpgga.num_sats)
-        print "WGS84:",lat,longs #WGS84
-        print "GCJ02:",transform(lat,longs) #GCJ02
-    
+            if GCJ02:
+                return transform(lat,longs)
+            else:
+                return (lat,longs)
+
+if __name__ == "__main__":
+    ser = serial.Serial("COM2",38400)
+
+    origin_position = ()
+    current_position = ()
+
+    origin_position = decodeNMEAStream(ser,GCJ02=True)
+    while True:
+        current_position = decodeNMEAStream(ser,GCJ02=True)
+        print current_position,
+        print "distance:",distance(origin_position,current_position)*1000,'m'
