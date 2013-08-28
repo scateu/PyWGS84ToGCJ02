88cda686e55a2450a765e192167bfd29fd8c57e6
Wang Kang <wangk@startest.net>
2013-08-28 22:23:07 +0800

    init

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ PyWGS84ToGCJ02
 ==============
 
 Python WGS84 To GCJ02 aka 火星坐标转换 
+
+关键词: wgtochina\_lb 地形图非线性保密处理技术
---
+++ WGS84ToGCJ02.py
@@ -0,0 +1,61 @@
+from __future__ import division
+from math import pi,sqrt,sin,cos
+
+# a python binding of https://on4wp7.codeplex.com/SourceControl/changeset/view/21483#353936
+
+# Krasovsky 1940
+#
+# a = 6378245.0, 1/f = 298.3
+# b = a * (1 - f)
+# ee = (a^2 - b^2) / a^2
+a = 6378245.0
+ee = 0.00669342162296594323
+
+# World Geodetic System ==> Mars Geodetic System
+def transform(wgLat, wgLon):
+    """
+    transform(latitude,longitude) , WGS84
+    return (latitude,longitude) , GCJ02
+    """
+    if (outOfChina(wgLat, wgLon)):
+        mgLat = wgLat
+        mgLon = wgLon
+        return
+    dLat = transformLat(wgLon - 105.0, wgLat - 35.0)
+    dLon = transformLon(wgLon - 105.0, wgLat - 35.0)
+    radLat = wgLat / 180.0 * pi
+    magic = sin(radLat)
+    magic = 1 - ee * magic * magic
+    sqrtMagic = sqrt(magic)
+    dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi)
+    dLon = (dLon * 180.0) / (a / sqrtMagic * cos(radLat) * pi)
+    mgLat = wgLat + dLat
+    mgLon = wgLon + dLon
+    return mgLat,mgLon
+
+def outOfChina(lat, lon):
+    if (lon < 72.004 or lon > 137.8347):
+        return True
+    if (lat < 0.8293 or lat > 55.8271):
+        return True
+    return False
+
+def transformLat(x, y):
+    ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * sqrt(abs(x))
+    ret += (20.0 * sin(6.0 * x * pi) + 20.0 * sin(2.0 * x * pi)) * 2.0 / 3.0
+    ret += (20.0 * sin(y * pi) + 40.0 * sin(y / 3.0 * pi)) * 2.0 / 3.0
+    ret += (160.0 * sin(y / 12.0 * pi) + 320 * sin(y * pi / 30.0)) * 2.0 / 3.0
+    return ret
+
+def transformLon(x, y):
+    ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * sqrt(abs(x))
+    ret += (20.0 * sin(6.0 * x * pi) + 20.0 * sin(2.0 * x * pi)) * 2.0 / 3.0
+    ret += (20.0 * sin(x * pi) + 40.0 * sin(x / 3.0 * pi)) * 2.0 / 3.0
+    ret += (150.0 * sin(x / 12.0 * pi) + 300.0 * sin(x / 30.0 * pi)) * 2.0 / 3.0
+    return ret
+
+if __name__ == "__main__":
+    WGS84_Lat = 39.990205
+    WGS84_Long = 116.327847
+    print transform(WGS84_Lat,WGS84_Long)
+
